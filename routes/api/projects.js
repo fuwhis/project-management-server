0b20cd9006f00aa66b2ecd0afef226be4a5218b2
steve.parker117@gmail.com <steve.parker117@gmail.com>
2020-04-13 05:41:11 +0700

    update api/projects

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var Project = require("../../models/Project");
 
 // create new project
-router.post('/project', function (req, res, next) {
+router.post('/projects/create-new', function (req, res, next) {
   Project.findById(req.payload.id).then(function (project) {
     if (!project) {
       return res.sendStatus(401);
@@ -10,7 +10,7 @@ router.post('/project', function (req, res, next) {
 
     var project = new Project();
 
-    project.name = req.body.project.name; 
+    project.name = req.body.project.name;
     project.description = req.body.project.description;
 
     project.save().then(function () {
@@ -19,4 +19,39 @@ router.post('/project', function (req, res, next) {
   });
 });
 
+// update project
+router.put('/projects/update/:id', (req, res) => {
+  // Validate request
+  if (!req.body.id) {
+    return res.status(400).send({
+      message: 'Project Id can not be null'
+    });
+  }
+
+  // Find project and update
+  Project.findByIdAndUpdate(req.params.id, {
+    name: req.body.name || '',
+    description: req.body.description
+  }, { new: true }).then(prj => {
+    if (!prj) {
+      return res.status(404).send({
+        message: 'Project not found'
+      });
+    }
+    res.send(prj);
+  }).catch(err => {
+    if (err.kind === 'ObjectId') {
+      return res.status(404).send({
+        message: 'Project not found with id ' + req.params.id
+      });
+    }
+    return res.status(500).send({
+      message: "Error updating project with id " + req.params.id
+    });
+  })
+});
+
+// assign member to project
+
+// show project detail (name, list member)
 module.exports = router;
