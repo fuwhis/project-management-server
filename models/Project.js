5ffd13a6e0eb29ba69521e4149ddb8ae78568198
steve.parker117@gmail.com <steve.parker117@gmail.com>
2020-04-13 05:40:32 +0700

    update model projects

--- models/Project.js
+++ models/Project.js
@@ -1,20 +1,9 @@
 var mongoose = require('mongoose');
 
 var ProjectSchema = new mongoose.Schema({
-  id: { type: String, lowercase: true, unique: true, match: [/^a-zA-Z0-9+$/, '111'], },
+  project_id: { type: String, lowercase: true, unique: true, match: [/^a-zA-Z0-9+$/, '111'], },
   name: { type: String, lowercase: false, unique: true, match: [/^A-Z0-9+$/, '222'] },
   description: String,
 }, { timestamps: true });
-
-ProjectSchema.methods.createNewPrj = function () {
-
-}
-
-ProjectSchema.methods.updatePrj = function (id) {
-  if (this.id.indexOf(id) === -1) {
-    // notify this prj not exist
-  }
-  return this.save();
-}
 
 module.exports = mongoose.model('Project', ProjectSchema); 
