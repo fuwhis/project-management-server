891ad0386530cdd0fc39266a08194d3bf27b27df
steve.parker117@gmail.com <steve.parker117@gmail.com>
2020-04-13 00:28:32 +0700

    update model

--- models/Member.js
+++ models/Member.js
@@ -9,29 +9,4 @@ var MemberSchema = new mongoose.Schema({
   birthdate: { type: Date, },
 }, { timestamps: true });
 
-MemberSchema.methods.toMemberJSON = function() {
-  return {
-    member_name: this.member_name,
-    phone: this.phone,
-    birthdate: this.birthdate
-  }
-}
-
-MemberSchema.methods.addMember = function () {
-  // xem lai
-  return {
-    member_id: this.member_id + 1,
-    member_name: this.member_name,
-    phone: this.phone,
-    birthdate: this.birthdate
-  }
-}
-//với lại cái này a nên dùng findOneAnđUpate
-MemberSchema.methods.updateMember = function (member_id) {
-  if (this.member_id.indexOf(member_id) === -1) {
-    // notify this mem not exist
-  }
-  return this.save();
-}
-
 module.exports = mongoose.model('Member', MemberSchema);
