5eb41b6981acc75bd24b6117bc1630f4942b8292
steve.parker117@gmail.com <steve.parker117@gmail.com>
2020-04-13 13:28:39 +0700

    Update model

--- models/User.js
+++ models/User.js
@@ -5,23 +5,14 @@ var jwt = require('jsonwebtoken');
 var secret = require('../config').secret;
 
 var UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    // lowercase: true,
-    // unique: true,
-    // required: [true, "can't be blank"],
-    // match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
-    // index: true,
-    trim: true,
-  },
+  username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true, trim: true },
+  email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
   phone: { type: Number },
   birthdate: { type: Date },
   password: {
     type: String,
-    // required: true,
+    required: true,
   }
 }, { timestamps: true });
-
-
 
 module.exports = mongoose.model('User', UserSchema);
